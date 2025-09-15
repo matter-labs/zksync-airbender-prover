25ecd1c0f2c1225ff37eaa3a4c8ef6d0e4bc9baf
AlexOnTheStorm <alexanderdanichev@gmail.com>
2025-09-15 13:07:15 +0000

    args in lib

--- crates/zksync_os_prover_service/src/lib.rs
+++ crates/zksync_os_prover_service/src/lib.rs
@@ -15,6 +15,44 @@ use zksync_airbender_execution_utils::{get_padded_binary, UNIVERSAL_CIRCUIT_VERI
 use zksync_os_fri_prover::create_proof;
 use zksync_sequencer_proof_client::{sequencer_proof_client::SequencerProofClient, ProofClient};
 
+/// Command-line arguments for the Zksync OS prover
+#[derive(Parser, Debug)]
+#[command(name = "Zksync OS Prover")]
+#[command(version = "1.0")]
+#[command(about = "Prover for Zksync OS", long_about = None)]
+pub struct Args {
+    /// Max SNARK latency in seconds (default value - 1 hour)
+    #[arg(long, default_value = "3600", conflicts_with = "max_fris_per_snark")]
+    max_snark_latency: Option<u64>,
+    /// Max amount of FRI proofs per SNARK (default value - 100)
+    #[arg(long, default_value = "100", conflicts_with = "max_snark_latency")]
+    max_fris_per_snark: Option<usize>,
+    /// Base URL for the proof-data server (e.g., "http://<IP>:<PORT>")
+    #[arg(short, long, default_value = "http://localhost:3124")]
+    pub base_url: String,
+    /// Enable logging and use the logging-enabled binary
+    #[arg(long)]
+    pub enabled_logging: bool,
+    /// Path to `app.bin`
+    #[arg(long)]
+    pub app_bin_path: Option<PathBuf>,
+    /// Circuit limit - max number of MainVM circuits to instantiate to run the block fully
+    #[arg(long, default_value = "10000")]
+    pub circuit_limit: usize,
+    /// Directory to store the output files for SNARK prover
+    #[arg(long)]
+    pub output_dir: String,
+    /// Path to the trusted setup file for SNARK prover
+    #[arg(long)]
+    pub trusted_setup_file: Option<String>,
+    /// Number of iterations (SNARK proofs) to generate before exiting
+    #[arg(long)]
+    pub iterations: Option<usize>,
+    /// Path to the output file for FRI proofs
+    #[arg(short, long)]
+    pub fri_path: Option<PathBuf>,
+}
+
 pub fn init_tracing() {
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     FmtSubscriber::builder().with_env_filter(filter).init();
--- crates/zksync_os_prover_service/src/main.rs
+++ crates/zksync_os_prover_service/src/main.rs
@@ -1,43 +1,5 @@
 use clap::Parser;
 
-/// Command-line arguments for the Zksync OS prover
-#[derive(Parser, Debug)]
-#[command(name = "Zksync OS Prover")]
-#[command(version = "1.0")]
-#[command(about = "Prover for Zksync OS", long_about = None)]
-pub struct Args {
-    /// Max SNARK latency in seconds (default value - 1 hour)
-    #[arg(long, default_value = "3600", conflicts_with = "max_fris_per_snark")]
-    max_snark_latency: Option<u64>,
-    /// Max amount of FRI proofs per SNARK (default value - 100)
-    #[arg(long, default_value = "100", conflicts_with = "max_snark_latency")]
-    max_fris_per_snark: Option<usize>,
-    /// Base URL for the proof-data server (e.g., "http://<IP>:<PORT>")
-    #[arg(short, long, default_value = "http://localhost:3124")]
-    pub base_url: String,
-    /// Enable logging and use the logging-enabled binary
-    #[arg(long)]
-    pub enabled_logging: bool,
-    /// Path to `app.bin`
-    #[arg(long)]
-    pub app_bin_path: Option<PathBuf>,
-    /// Circuit limit - max number of MainVM circuits to instantiate to run the block fully
-    #[arg(long, default_value = "10000")]
-    pub circuit_limit: usize,
-    /// Directory to store the output files for SNARK prover
-    #[arg(long)]
-    pub output_dir: String,
-    /// Path to the trusted setup file for SNARK prover
-    #[arg(long)]
-    pub trusted_setup_file: Option<String>,
-    /// Number of iterations (SNARK proofs) to generate before exiting
-    #[arg(long)]
-    pub iterations: Option<usize>,
-    /// Path to the output file for FRI proofs
-    #[arg(short, long)]
-    pub fri_path: Option<PathBuf>,
-}
-
 #[tokio::main]
 pub async fn main() {
     let args = zksync_os_prover_service::Args::parse();
