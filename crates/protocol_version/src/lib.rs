e65a6de849491fb1f20ecaf864e15df649e6eb77
EmilLuta <virgil.luta@gmail.com>
2025-11-03 13:23:57 +0100

    Add docs for protocol_version crate

--- crates/protocol_version/src/lib.rs
+++ crates/protocol_version/src/lib.rs
@@ -1,19 +1,31 @@
 use std::fmt::{self, Debug};
 
+/// Represents a specific protocol version supported by the prover, from prover's perspective.
 struct ProtocolVersion {
-    pub vk_hash: VerificationKeyHash,
-    pub airbender_version: AirbenderVersion,
-    pub zksync_os_version: ZkSyncOSVersion,
-    pub zkos_wrapper: ZkOsWrapperVersion,
-    pub bin_md5sum: BinMd5sum,
+    /// verification key hash identifying this protocol version
+    vk_hash: VerificationKeyHash,
+    /// version of airbender used
+    /// NOTE: this can be inferred from vk_hash, but we keep it here for easier cross-checking
+    airbender_version: AirbenderVersion,
+    /// version of zksync os used
+    /// NOTE: this can be inferred from vk_hash, but we keep it here for easier cross-checking
+    zksync_os_version: ZkSyncOSVersion,
+    /// version of zkos wrapper used
+    /// NOTE: this can be inferred from vk_hash, but we keep it here for easier cross-checking
+    zkos_wrapper: ZkOsWrapperVersion,
+    /// md5sum of the prover binary used for proving
+    /// NOTE: in the future we may want to support multiple binaries (such as debug mode)
+    /// NOTE2: this can be inferred from zksync_os_version, but we keep it here for easier cross-checking
+    bin_md5sum: BinMd5Sum,
 }
 
 struct VerificationKeyHash(&'static str);
 struct AirbenderVersion(&'static str);
 struct ZkSyncOSVersion(&'static str);
 struct ZkOsWrapperVersion(&'static str);
-struct BinMd5sum(&'static str);
+struct BinMd5Sum(&'static str);
 
+/// Corresponds to server's execution_version 3 (or v1.1)
 const V3: ProtocolVersion = ProtocolVersion {
     vk_hash: VerificationKeyHash(
         "0x6a4509801ec284b8921c63dc6aaba668a0d71382d87ae4095ffc2235154e9fa3",
@@ -21,9 +33,10 @@ const V3: ProtocolVersion = ProtocolVersion {
     airbender_version: AirbenderVersion("0.5.0"),
     zksync_os_version: ZkSyncOSVersion("0.0.26"),
     zkos_wrapper: ZkOsWrapperVersion("0.5.0"),
-    bin_md5sum: BinMd5sum("fd9fd6ebfcfe7b3d1557e8a8b8563dd6"),
+    bin_md5sum: BinMd5Sum("fd9fd6ebfcfe7b3d1557e8a8b8563dd6"),
 };
 
+/// Represents the set of supported protocol versions by this prover implementation.
 pub struct SupportedProtocolVersions {
     versions: Vec<ProtocolVersion>,
 }
@@ -50,6 +63,7 @@ impl Debug for SupportedProtocolVersions {
 }
 
 impl SupportedProtocolVersions {
+    /// Checks if the given VK hash is supported.
     pub fn contains(&self, vk_hash: &str) -> bool {
         for version in &self.versions {
             if version.vk_hash.0 == vk_hash {
@@ -59,6 +73,7 @@ impl SupportedProtocolVersions {
         false
     }
 
+    /// Returns the list of supported VK hashes as strings.
     pub fn vk_hashes(&self) -> Vec<String> {
         self.versions
             .iter()
