299d9504c25293250fc4ec5d14909ee1d9ee56b4
perekopskiy <53865202+perekopskiy@users.noreply.github.com>
2025-11-06 17:08:43 +0200

    feat: make Args::disable_zk public (#99)
    Makes `Args::disable_zk` public so args can be constructed externally as
    this is the only way to use prover service as a lib

--- crates/zksync_os_prover_service/src/lib.rs
+++ crates/zksync_os_prover_service/src/lib.rs
@@ -56,7 +56,7 @@ pub struct Args {
     pub fri_path: Option<PathBuf>,
     /// Disable ZK for SNARK proofs
     #[arg(long, default_value_t = false)]
-    disable_zk: bool,
+    pub disable_zk: bool,
 }
 
 pub fn init_tracing() {
